c78e929b4eef4632556c602467fe839c963b1b41
varshithpawarhr <varshithpawarhr@gmail.com>
2024-10-11 22:26:04 +0530

    navbar--mobile view done

--- src/components/Herosection.tsx
+++ src/components/Herosection.tsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
-import personImage from "../assets/ca.png"; // replace with correct image path // replace with your logo path
+import personImage from "../assets/ca.png"; // replace with correct image path
+
 const App = () => {
   return (
-    <div className="bg-[#ede8f5] min-h-screen flex flex-col justify-center items-center">
+    <div className="bg-[#ede8f5] min-h-screen flex flex-col justify-between">
       <header className="absolute top-4 left-4 flex items-center gap-4"></header>
 
-      <main className="flex flex-col-reverse lg:flex-row justify-center items-center lg:gap-20 gap-10 px-6 lg:px-24 py-10">
+      <main className="flex flex-col-reverse lg:flex-row justify-center items-center lg:gap-20 gap-10 px-6 lg:px-24 py-10 flex-grow">
         {/* Left section */}
         <div className="flex flex-col gap-6 text-center lg:text-left lg:w-1/2">
           <h1 className="text-4xl lg:text-5xl font-bold text-gray-800">
@@ -46,8 +47,13 @@ const App = () => {
         </div>
       </main>
 
-      <div className="border-t border-gray-300 py-2">
-        <span className="text-sm text-gray-500">T4Tax-Tax Made Easy</span>
+      {/* Partial underline effect at the bottom of the hero section */}
+      <div className="text-center pb-4">
+        <div className=" border-t border-gray-300 w-1/4 mx-auto mt-2 py-1">
+          <span className="text-sm text-gray-500 mt-1">
+            T4Tax-Tax Made Easy
+          </span>
+        </div>
       </div>
     </div>
   );
